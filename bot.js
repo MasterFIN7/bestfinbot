3fe4ab1e5788403abd48ccd1be2e9599e5ba31d3
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-23 13:32:02 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const discord = require("discord.js");
+
+const PREFIX = "f!";
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+  console.log("Ready");
+  bot.user.setGame("Use f!help");
+});
+
+bot.on("message", function(message) {
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(PREFIX)) return;
+
+  var args = message.content.substring(PREFIX.length).split(" ");
+  
+  switch (args[0].toLowerCase()) {
+    case "ping":
+      message.channel.sendMessage("pong");
+      break;
+    case "info":
+      message.channel.sendMessage("Olen PerunaCraftin Discord botti.");
+      break;
+    default:
+      message.channel.sendMessage("Invalid Command");
+  }
+});
+
+bot.login(process.env.BOT_TOKEN);
